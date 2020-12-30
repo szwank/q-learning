b790282560915705b924c3fdd2d076c2d8360e00
lpankiewicz <lukasz.pankiewicz@profil-software.com>
2020-12-30 12:12:31 +0100

    Update pendulum agent parameters

--- DQN_inverted_pendulum.py
+++ DQN_inverted_pendulum.py
@@ -6,10 +6,12 @@ from networks import get_dense_model
 
 learner = QLearner(model=get_dense_model((4, 4), 2, 0.00025),
                    env_name='CartPole-v1',
-                   replay_size=1000000,
-                   replay_start_size=50000,
-                   final_exploration_frame=1000000,
+                   replay_size=100000,
+                   replay_start_size=10000,
+                   final_exploration_frame=100000,
+                   batch_size=64,
+                   max_game_length=2000
                    )
 
-learner.train(plot=True)
+learner.train(n_frames=1000000, plot=True, render_period=10)
 
