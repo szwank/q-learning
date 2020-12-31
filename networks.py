d24f9ea377b848b95ef8d55b551963940702b773
lpankiewicz <lukasz.pankiewicz@profil-software.com>
2020-12-31 11:18:56 +0100

    Update get_dense_model model. Change RMSprop parameters. Split one hidden layer into two with the same number of neurons

--- networks.py
+++ networks.py
@@ -59,11 +59,12 @@ def get_dense_model(input_size, n_actions, lr):
     actions_input = layers.Input(n_actions)
 
     x = layers.Flatten()(state_input)
-    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dense(32, activation='relu')(x)
+    x = layers.Dense(32, activation='relu')(x)
     x = layers.Dense(n_actions, activation='linear')(x)
     x = layers.Multiply()([x, actions_input])
 
     model = models.Model(inputs=[state_input, actions_input], outputs=x)
-    optimizer = optimizers.RMSprop(lr=lr, rho=0.95, epsilon=0.01, momentum=0.95)
+    optimizer = optimizers.RMSprop(lr=lr, momentum=0.95)
     model.compile(optimizer, loss='mse')
     return model
